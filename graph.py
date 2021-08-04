4743c85fef3bf079a4f330ee072c46ece471bd0a
Randall Hand <randall.hand@gmail.com>
2021-08-04 13:12:00 -0400

    Basic graph functional

--- graph.py
+++ graph.py
@@ -13,21 +13,70 @@ output.write("""<html>
 <body>
 	<canvas id="myChart"></canvas>
 <script>
+chartData = {
+	datasets: [
 """)
 
 config = configparser.ConfigParser()
 config.read('rvwhisper.ini')
+
+graphPeriod = config.get('GRAPH', 'period', fallback="-30 days")
+print("Graphing data over period: %s" % graphPeriod)
+
 for db in config['GRAPH']['db'].split(','):
 	title = config.get('GRAPH', db, fallback = db)
 	print("Reading %s (%s)" % (db, title))
+
+	# Open the Database
+	conn = None
+	try:
+		conn = sqlite3.connect(db)
+	except sqlite3.Error as e:
+		print(e)
+
+
+	# Retrieve the list of suitable fields for visualization
+	fields = []
+	c = conn.cursor();
+	try:
+		c.execute('SELECT fieldname,count(*) FROM data WHERE timestamp > strftime("%%s", datetime("now", "%s")) GROUP BY fieldname' % graphPeriod)
+		rows = c.fetchall()
+		for row in rows:
+			fields.append(row[0])
+	except sqlite3.Error as e:
+		print(e)
+
+	print("-> Found fields [%s]" % fields)	
+
+	# For each field, retrieve the data for the last period
+	for field in fields:
+		try:
+			output.write("""{ type: 'scatter',
+					  label: '%s - %s', 
+					  data: [""" % (title, field))
+	
+			c.execute('SELECT timestamp,value FROM data WHERE timestamp > strftime("%%s", datetime("now", "%s")) AND fieldname = "%s"' % (graphPeriod, field))
+			rows = c.fetchall()
+			dataString = []
+			for row in rows:
+				dataString.append('{x: %s, y: %s}' % (row[0], row[1]))
+			output.write(','.join(dataString))
+			output.write("""] }, """)
+		except sqlite3.Error as e:
+			print(e)
+
+	# Close the database, if it was opened successfully
+	if(conn):
+		conn.close()
 
 
 
 # Write out the HTML Footer
 output.write("""
+  {} ] };
   const config = {
-    type: 'line',
-    data,
+    type: "scatter",
+    data: chartData,
     options: {}
   };
   var myChart = new Chart(
