0c6b9dc3e2a9e96974feee4aab685a8a595f4972
Randall Hand <randall.hand@gmail.com>
2021-08-04 14:18:19 -0400

    Special handling for Doors

--- graph.py
+++ graph.py
@@ -57,13 +57,23 @@ for db in config['GRAPH']['db'].split(','):
 			output.write("""{ type: 'scatter',
 					  label: '%s - %s', 
 					  yAxisID: '%s',
+					  showLine: true,
 					  data: [""" % (title, field, field))
 	
 			c.execute('SELECT timestamp,value FROM data WHERE timestamp > strftime("%%s", datetime("now", "%s")) AND fieldname = "%s"' % (graphPeriod, field))
 			rows = c.fetchall()
 			dataString = []
-			for row in rows:
-				dataString.append('{x: %s, y: %s}' % (row[0], row[1]))
+			if (field == "DoorState"):
+				# Special handling for doors.. Doors have "Just Opened", "Just Closed", and "Still Closed" and "Still Open"
+				# We'll convert that to a binary for easier rendering.
+				for row in rows:
+					if ("Closed" in row[1]):
+						dataString.append('{x: %s, y: 0}' % (row[0]))
+					else:
+						dataString.append('{x: %s, y: 1}' % (row[0]))
+			else:
+				for row in rows:
+					dataString.append('{x: %s, y: %s}' % (row[0], row[1]))
 			output.write(','.join(dataString))
 			output.write("""],
 				borderColor: '%s',
