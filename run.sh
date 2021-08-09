981d7cf8de5e6655a13118786c987f911daad2d8
Randall Hand <randall.hand@gmail.com>
2021-08-09 13:59:56 -0400

    Perform temp analyssi

--- run.sh
+++ run.sh
@@ -7,3 +7,4 @@ python3 graph.py
 # Perform Analysis
 cd analysis
 ./slope.py -i ../Engine\ battery.db -f Volts -w 10 -o /var/www/html/graph/volts.html
+./delta.py -i ../Indoor.db -w ../weather.db -o /var/www/html/graph/temp.html
